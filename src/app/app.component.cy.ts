d8b69a9d97243fa94bb2e07953524b526df62ad1
Julian Zankl <julian.zankl@clouddna.at>
2024-05-05 13:02:17 +0200

    PGL-71: added cypress

---
+++ src/app/app.component.cy.ts
@@ -0,0 +1,8 @@
+import {AppComponent} from "./app.component";
+
+describe('App Component', () => {
+    it('should include heading', () => {
+        cy.mount(AppComponent);
+        cy.get('h2').should('contain.text', 'Heading');
+    })
+});
