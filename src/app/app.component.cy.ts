2f31ad2b5f78a4325ec79dc17987f100f5bdc33c
Marco_Ivantschitz <ivantschitz@hotmail.com>
2024-06-03 22:56:04 +0200

    PGL-22: updated app.component.cy.ts file

--- src/app/app.component.cy.ts
+++ src/app/app.component.cy.ts
@@ -1,8 +1,16 @@
-import {AppComponent} from "./app.component";
+import { AppComponent } from './app.component';
 
 describe('App Component', () => {
+    before(() => {
+        // Vor jedem Test die AppComponent rendern
+        cy.visit('/');
+        cy.get('app-root').then(() => {
+            cy.mount(AppComponent);
+        });
+    });
+
     it('should include heading', () => {
-        cy.mount(AppComponent);
-        cy.get('h2').should('contain.text', 'Heading');
-    })
+        // Testen, ob die Überschrift vorhanden ist
+        cy.get('h1').should('be.visible').and('contain.text', 'Heading');
+    });
 });
