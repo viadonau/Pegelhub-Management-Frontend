bb1d715afff178a407c853295b192de9c294c5fd
Marco_Ivantschitz <ivantschitz@hotmail.com>
2024-06-03 22:49:59 +0200

    PGL-22: added app.module.ts

--- src/app/core/index.ts
+++ src/app/core/index.ts
@@ -1,4 +1,1 @@
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 export { providePaginatorTranslations } from './paginator-translations';
-
-export { BrowserAnimationsModule }; // Exportiere BrowserAnimationsModule für die Verwendung in anderen Modulen
