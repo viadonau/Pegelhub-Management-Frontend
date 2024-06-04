619dbb4151ecde496198318558ca0c864c90917a
Julian Zankl <2210859018@fh-burgenland.at>
2024-06-04 20:36:43 +0200

    test: updated cypress config and added basic test

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:4200',
+    specPattern: 'cypress/e2e/**/*.cy.ts',
+    supportFile: false,
+  },
   component: {
     devServer: {
       framework: 'angular',
