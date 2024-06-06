2fc9b1ab61a863c1cc36bae8dec56534d1221d22
Julian Zankl <2210859018@fh-burgenland.at>
2024-06-06 17:04:00 +0200

    ci: adjusted baseUrl for e2e tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:4200',
+    baseUrl: 'http://localhost:4202',
     specPattern: 'cypress/e2e/**/*.cy.ts',
     supportFile: false,
   },
